a680aee37f590f08e720d9735ea7d2186080f601
SATISH <chaudhar.satish@gmail.com>
2020-06-26 12:17:54 +0530

    Update data_cleaning.py

--- ds_salary_proj-master/data_cleaning.py
+++ ds_salary_proj-master/data_cleaning.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 df = pd.read_csv('glassdoor_jobs.csv')
 
-#salary parsing 
+#SalaryParsing 
 
 df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
 df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
